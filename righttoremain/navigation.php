91221cec1dcd98b0d64feb24c91c0b5807c1ada1
rcopestake <richard@redink.co.uk>
2021-02-15 14:16:53 +0000

    amds of 15 Feb

--- righttoremain/navigation.php
+++ righttoremain/navigation.php
@@ -14,6 +14,19 @@
     $possible_outcomes = get_field( "background_colour", 357 );
     $fresh_claim = get_field( "background_colour", 360 );
     $fresh_outcomes = get_field( "background_colour", 363 );
+
+    $ayslum_menu_color =  get_field( "menu_text_colour", 137 );
+    $welfare_menu_color = get_field( "menu_text_colour", 220 );
+    $local_menu_color = get_field( "menu_text_colour", 209 );
+    $evidence_menu_color = get_field( "menu_text_colour", 319 );
+    $substantive_interview_menu_color = get_field( "menu_text_colour", 324 );
+    $positive_decision_menu_color = get_field( "menu_text_colour", 333 );
+    $refusal_menu_color = get_field( "menu_text_colour", 339 );
+    $submit_appeal_menu_color = get_field( "menu_text_colour", 346 );
+    $appeal_hearing_menu_color = get_field( "menu_text_colour", 353 );
+    $possible_outcomes_menu_color = get_field( "menu_text_colour", 357 );
+    $fresh_claimv = get_field( "menu_text_colour", 360 );
+    $fresh_outcomes_menu_color = get_field( "menu_text_colour", 363 );
 	
 	
 	$pages = get_pages(array(
@@ -48,54 +61,75 @@
 
         <div class="nav-item-container">
             <a href="/claim-asylum">
-                <div class="nav-item nav-item-1" style="background-color:<?php echo $asylum; ?>">
+                <div class="nav-item nav-item-1"
+                    style="background-color:<?php echo $asylum; ?>; color:<?php echo $ayslum_menu_color; ?>">
                     Claim Asylum
                 </div>
             </a>
             <a href="/welfare-interview">
-                <div class="nav-item nav-item-2" style="background-color:<?php echo $welfare; ?>">
+                <div class="nav-item nav-item-2"
+                    style="background-color:<?php echo $welfare; ?>; color:<?php echo $welfare_menu_color; ?>">
                     Welfare Interview</div>
             </a>
             <a href="/local-authority-care">
-                <div class="nav-item nav-item-3" style="background-color:<?php echo $local; ?>">Local Authority Care
+                <div class="nav-item nav-item-3"
+                    style="background-color:<?php echo $local; ?>; color:<?php echo $local_menu_color; ?>">Local
+                    Authority Care
                 </div>
             </a>
             <a href="/statement-of-evidence-form">
-                <div class="nav-item nav-item-4" style="background-color:<?php echo $evidence; ?>">Statement of Evidence
+                <div class="nav-item nav-item-4"
+                    style="background-color:<?php echo $evidence; ?>; color:<?php echo $evidence_menu_color; ?>">
+                    Statement of Evidence
                     Form</div>
             </a>
             <a href="/substantive-interview">
-                <div class="nav-item nav-item-5" style="background-color:<?php echo $substantive_interview; ?>">
+                <div class="nav-item nav-item-5"
+                    style="background-color:<?php echo $substantive_interview; ?>; color:<?php echo $substantive_interview_menu_color; ?>">
                     Substantive
                     Interview</div>
             </a>
             <a href="/home-office-positive-decision">
-                <div class="nav-item nav-item-6" style="background-color:<?php echo $positive_decision; ?>">Home Office
+                <div class="nav-item nav-item-6"
+                    style="background-color:<?php echo $positive_decision; ?>; color:<?php echo $positive_decision_menu_color; ?>">
+                    Home Office
                     positive
                     decision</div>
             </a>
             <a href="/home-officere-refusal">
-                <div class="nav-item nav-item-7" style="background-color:<?php echo $refusal; ?>">Home Office refusal
+                <div class="nav-item nav-item-7"
+                    style="background-color:<?php echo $refusal; ?>; color:<?php echo $refusal_menu_color; ?>">Home
+                    Office refusal
                 </div>
             </a>
             <a href="/submit-appeal">
-                <div class="nav-item nav-item-8" style="background-color:<?php echo $submit_appeal; ?>">Submit appeal
+                <div class="nav-item nav-item-8"
+                    style="background-color:<?php echo $submit_appeal; ?>; color:<?php echo $submit_appeal_menu_color; ?>">
+                    Submit appeal
                 </div>
             </a>
             <a href="/appeal-hearing">
-                <div class="nav-item nav-item-9" style="background-color:<?php echo $appeal_hearing; ?>">Appeal hearing
+                <div class="nav-item nav-item-9"
+                    style="background-color:<?php echo $appeal_hearing; ?>; color:<?php echo $appeal_hearing_menu_color; ?>">
+                    Appeal hearing
                 </div>
             </a>
             <a href="/possible-outcomes-after-appeal-hearing">
-                <div class="nav-item nav-item-10" style="background-color:<?php echo $possible_outcomes; ?>">Outcomes
+                <div class="nav-item nav-item-10"
+                    style="background-color:<?php echo $possible_outcomes; ?>; color:<?php echo $possible_outcomes_menu_color; ?>">
+                    Outcomes
                     after
                     appeal hearing</div>
             </a>
             <a href="/fresh-claim">
-                <div class="nav-item nav-item-11" style="background-color:<?php echo $fresh_claim; ?>">Fresh claim</div>
+                <div class="nav-item nav-item-11"
+                    style="background-color:<?php echo $fresh_claim; ?>; color:<?php echo $fresh_claim_menu_color; ?>">
+                    Fresh claim</div>
             </a>
             <a href="/possible-outcomes-from-a-fresh-claim">
-                <div class="nav-item nav-item-12" style="background-color:<?php echo $fresh_outcomes; ?>">Outcomes from
+                <div class="nav-item nav-item-12"
+                    style="background-color:<?php echo $fresh_outcomes; ?>; color:<?php echo $fresh_outcomes_menu_color; ?>">
+                    Outcomes from
                     a fresh claim</div>
             </a>
         </div>
