8db32bba2b745b75cf8faa6e38ae3a2a6138df7b
rcopestake <richard@redink.co.uk>
2021-03-02 12:39:33 +0000

    mobile menu back to side of page

--- righttoremain/utilities.php
+++ righttoremain/utilities.php
@@ -5,7 +5,7 @@
     <ul style="background-color:<?php the_field('background_colour'); ?>">
         <li><a href="/">Home</a></li>
         <li><a href="/overview">Overview</a></li>
-        <li><a href="/about">About</a></li>
+        <li><a href="/how-to-use">How to use</a></li>
         <li><a href="/people-who-can-help">People who can help</a></li>
     </ul>
 </nav>
